76f1d25a890a3f5740539735441cf4f72b9a72b7
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-18 20:07:48 -0500

    updated the user schema

--- models/User.js
+++ models/User.js
@@ -1,43 +1,48 @@
 const { Schema, model } = require('mongoose');
+const User = require('./Reaction');
 
-// // Schema to create User model
-// const userSchema = new Schema(
-//   {
-//     first: String,
-//     last: String,
-//     age: Number,
-//     videos: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: 'Video',
-//       },
-//     ],
-//   },
-//   {
-//     // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-//     // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
-//     toJSON: {
-//       virtuals: true,
-//     },
-//     id: false,
-//   }
-// );
+// **Schema Settings**:
+// Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
 
-// // Create a virtual property `fullName` that gets and sets the user's full name
-// userSchema
-//   .virtual('fullName')
-//   // Getter
-//   .get(function () {
-//     return `${this.first} ${this.last}`;
-//   })
-//   // Setter to set the first and last name
-//   .set(function (v) {
-//     const first = v.split(' ')[0];
-//     const last = v.split(' ')[1];
-//     this.set({ first, last });
-//   });
+// Schema to create Post model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim:true,
+      unique:true,
+      },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+       match:[/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, `please add a valid e-mail address`]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    // thoughts: [Thought],
+    // friends: [User],
+  },
+);
 
-// // Initialize our User model
-// const User = model('user', userSchema);
+friendCount
+  .virtual('getFriends')
+    .get(function () {
+    return this.friends.length;
+  });
+
+
+const User = model('user', userSchema);
 
 module.exports = User;
