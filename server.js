79cfbd122478be548246dab810817f9c53bad967
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-22 16:09:16 -0500

    I believe I cleaned up the connection and server.js

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-//require express and mongodb
+//require express and mongodb and the connection from config
 const express = require('express');
 const mongodb = require('mongodb').MongoClient;
+const connection=require('./config/connection')
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-//for some readon I have to use 0.0.0.0 to connect to the local instance of Mongodb; added db name requirement
-let dbName = "socialMedia";
-const connectionStringURI = `mongodb://0.0.0.0:27017/${dbName}`;
 
-//a variable to hold the connection
-let db;
-
-// Creates a connection to a MongoDB instance and returns the reference to the database (refer to unit 18 activity 4 for more notes)
-mongodb.connect(connectionStringURI,  { useNewUrlParser: true, useUnifiedTopology: true },
+// Creates a connection to a MongoDB instance (refer to unit 18 activity 4 for more notes)
+mongodb.connect(connection,
   (err, client) => {
-    db = client.db();
     app.listen(port, () => {
       console.log(`Listening at http://localhost:${port}`);
     });
   }
 );
 
-// Built in Express function that parses incoming requests to JSON
 app.use(express.json());
+
+
+
+
+//--------------- IGNORE BELOW
+
+//for some reason I have to use 0.0.0.0 to connect to the local instance of Mongodb; added db name requirement
+// let dbName = "socialMedia";
+// const connectionStringURI = `mongodb://0.0.0.0:27017/${dbName}`;
+
+//a variable to hold the connection
+// let db;
+// db = client.db();
