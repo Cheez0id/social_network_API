6e217bd2dd8d80aed4ca3b310e619c8f7c00a1b5
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-22 19:31:26 -0500

    Added a post that creates a user with the user schema!

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //working code
 const express = require('express');
-const mongodb = require('mongodb').MongoClient;
+//the below is doing jack
+// const mongodb = require('mongodb').MongoClient;
 const db = require('./config/connection');
-const newUser = require('./models/User');
+//right now, this is doing Jack.
+const User = require('./models/User');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -29,12 +31,36 @@ app.post('/newUser', (req, res) => {
   );
 });
 
+//shows things that are added via above post
 app.get('/userList', (req, res) => {
   db.collection('user')
     .find({})
     .toArray((err, results) => {
       if (err) throw err;
       res.send(results);
+    });
+  });
+
+//to try and create a new user
+app.post('/createUser', (req, res) => {
+  const newUser = new User({username: req.body.username, email: req.body.email});
+  newUser.save();
+  if(newUser){
+    res.status(201).json(newUser);
+  } else {
+    console.log('Uh Oh, something went wrong');
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+//shows an empty array
+  app.get('/lookAtUsers', (req, res) =>{
+    User.find({}, (err, result) => {
+      if (err) {
+        res.status(500).send({ message: 'Internal Server Error' });
+      } else {
+        res.status(200).json(result);
+      }
     });
   });
 
